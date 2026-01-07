75b3f490be9320d49f08eac58e881e176c1b9063
Gibran Hemani <g.hemani@bristol.ac.uk>
2026-01-07 18:15:41 +0000

    upd

--- R/functions.r
+++ R/functions.r
@@ -9,6 +9,25 @@ my.write.table <- function(x, filename) {
 }
 
 #' Extract participant ID from TCGA barcode
+#' 
+#' The format of sample identifiers/barcodes is described here:
+#' https://docs.gdc.cancer.gov/Encyclopedia/pages/TCGA_Barcode/
+#'
+#' Here is a summary:
+#' e.g. TCGA-3C-AAAU-01A-11D-A41Q-05
+#'   project TCGA
+#'   tissue source site 3C
+#'   participant AAAU
+#'   sample 01 (01-09 tumor, 10-19 normal, 20-29 controls)
+#'   vial A
+#'   portion 11
+#'   analyte D (as in DNA)
+#'   plate A41Q
+#'   analysis center 05
+#'
+#' The following function extracts the participant identifier
+#' from a sample id/barcode.
+#'
 #' @param id TCGA barcode
 #' @return participant ID
 #' @export
@@ -17,6 +36,25 @@ extract.participant <- function(id) {
 }
 
 #' Extract tissue code from TCGA barcode
+#' 
+#' The format of sample identifiers/barcodes is described here:
+#' https://docs.gdc.cancer.gov/Encyclopedia/pages/TCGA_Barcode/
+#'
+#' Here is a summary:
+#' e.g. TCGA-3C-AAAU-01A-11D-A41Q-05
+#'   project TCGA
+#'   tissue source site 3C
+#'   participant AAAU
+#'   sample 01 (01-09 tumor, 10-19 normal, 20-29 controls)
+#'   vial A
+#'   portion 11
+#'   analyte D (as in DNA)
+#'   plate A41Q
+#'   analysis center 05
+#'
+#' The following function extracts the participant identifier
+#' from a sample id/barcode.
+
 #' @param id TCGA barcode
 #' @return tissue code
 #' @export
