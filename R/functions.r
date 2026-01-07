e31fe7eee93452de31a79369cd3957e5fca21e72
Gibran Hemani <g.hemani@bristol.ac.uk>
2026-01-07 18:02:35 +0000

    updated

--- R/functions.r
+++ R/functions.r
@@ -1,27 +1,47 @@
+#' Write table
+#' @param x data frame to write
+#' @param filename output file name
+#' @return none
+#' @export
 my.write.table <- function(x, filename) {
   cat("saving", basename(filename), "...\n")
   write.table(x, file = filename, row.names = T, col.names = T, sep = "\t")
 }
 
+#' Extract participant ID from TCGA barcode
+#' @param id TCGA barcode
+#' @return participant ID
+#' @export
 extract.participant <- function(id) {
   sub("TCGA-[^-]+-([^-]+)-.*", "\\1", id)
 }
 
+#' Extract tissue code from TCGA barcode
+#' @param id TCGA barcode
+#' @return tissue code
+#' @export
 extract.tissue <- function(id) {
   sub("TCGA-[^-]+-[^-]+-([0-9]+)[^-]+-.*", "\\1", id)
 }
 
-extract.file <- function(tar.file, extract.file, new.file) {
+#' Extract a specific file from a tar archive
+#' @param tar.file path to tar file
+#' @param extract.file file to extract from tar archive
+#' @param new.file output file name
+#' @param resultsdir directory to extract files to
+#' @return none
+#' @export
+extract.file <- function(tar.file, extract.file, new.file, resultsdir) {
   # get file path to extracted file
   x.file <-
     grep(extract.file,
-      untar(tar.file, list = T),
+      utils::untar(tar.file, list = T),
       value = T
     )
     
   # extract the tar file
   cat("Extracting", tar.file, "to", new.file, "\n")
-  untar(tar.file, exdir=resultsdir, extras="--no-same-owner")
+  utils::untar(tar.file, exdir=resultsdir, extras="--no-same-owner")
   x.file = file.path(resultsdir,x.file)
 
   # move the data to named output
@@ -29,9 +49,4 @@ extract.file <- function(tar.file, extract.file, new.file) {
 
   # remove untared directory
   unlink(dirname(x.file), recursive = TRUE)
-}
-
-my.write.table <- function(x, filename) {
-  cat("saving", basename(filename), "...\n")
-  write.table(x, file = filename, row.names = T, col.names = T, sep = "\t")
 }
